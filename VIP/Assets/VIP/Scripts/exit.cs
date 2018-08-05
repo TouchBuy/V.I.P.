08725bfdd7c161d35d5c4eb76fb8077fa25c0419
terfno <sueda.takahito@gmail.com>
2018-08-05 16:00:59 +0900

    Exit実装できた！！！！！！！！！！！！！！！！！うぇーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーい↑↑↑↑↑↑↑↑↑↑↑↑↑

---
+++ VIP/Assets/VIP/Scripts/exit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour {
+	public void OnClick(){
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello VIP!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
