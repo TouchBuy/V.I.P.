975ecae55c4648340b6f637556a35994c72b755a
glatan <glatan.edu@gmail.com>
2018-08-05 17:11:53 +0900

    商品データがないことには進められないので中断

---
+++ VIP/Assets/VIP/Scripts/OVRCameraRig_byKeyboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRCameraRig_byKeyboard : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<OVRCameraRig>().transform.Rotate(GetComponent<OVRCameraRig>().transform.right, -1f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<OVRCameraRig>().transform.Rotate(GetComponent<OVRCameraRig>().transform.right, 1f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<OVRCameraRig>().transform.Rotate(GetComponent<OVRCameraRig>().transform.up, -1f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<OVRCameraRig>().transform.Rotate(GetComponent<OVRCameraRig>().transform.up, 1f);
+        }
+    }
+}
