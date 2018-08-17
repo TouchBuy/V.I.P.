66720c19eb8fc3ceb767e1d74c97ba72969ea6a0
terfno <sueda.takahito@gmail.com>
2018-08-17 17:13:31 +0900

    fix_view update scripts add force

--- VIP/Assets/VIP/Scripts/view_swiipemove.cs
+++ VIP/Assets/VIP/Scripts/view_swiipemove.cs
@@ -6,15 +6,18 @@ public class view_swiipemove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Rigidbody car = GetComponent<Rigidbody>();
         if(OVRInput.Get(OVRInput.Button.Left)){
-            transform.Rotate(0, (float)-0.5, 0);
+            //transform.Rotate(0, (float)-0.5, 0);
+            car.AddForce(Vector3.forward * 0.1f,ForceMode.Impulse);
         }else if(OVRInput.Get(OVRInput.Button.Right)){
-            transform.Rotate(0, (float)0.5, 0);
+            //transform.Rotate(0, (float)0.5, 0);
+            car.AddForce(Vector3.forward * 0.1f, ForceMode.Impulse);
         }
 	}
 }
