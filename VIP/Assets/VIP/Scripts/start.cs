61798c64fe1f5a2d569082c430af15f553edfc76
glatan <glatan.edu@gmail.com>
2018-08-18 01:29:46 +0900

    fix

---
+++ VIP/Assets/VIP/Scripts/start.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+
+    //startが押されたらカテゴリ一覧に飛ぶ
+    public void OnClick() {
+        SceneManager.LoadScene("Category");
+    }
+}
